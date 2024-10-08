dae93b27d1d302baafa138b3a8c9a0dfaf5c0cca
Pratham Raj <rjpratham04@gmail.com>
2024-10-08 15:15:20 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -8,20 +8,19 @@ class Solution(object):
         mapping = {')': '(', '}': '{', ']': '['}
         
         for char in s:
-            if char in mapping:  # It's a closing bracket
+            if char in mapping: 
                 top_element = stack.pop() if stack else '#' 
                 if mapping[char] != top_element:
                     return False
-            else:  # It's an opening bracket
+            else:
                 stack.append(char)
         
-        return not stack  # Return True if the stack is empty (all brackets matched)
+        return not stack 
 
-# Example usage
 sol = Solution()
-print(sol.isValid("()"))      # Output: True
-print(sol.isValid("()[]{}"))  # Output: True
-print(sol.isValid("(]"))      # Output: False
+print(sol.isValid("()"))     
+print(sol.isValid("()[]{}"))
+print(sol.isValid("(]"))   
 
 
 
