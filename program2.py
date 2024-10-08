6b9648d4fa17e786eb2ad2668966cd0c43f6d633
Pratham Raj <rjpratham04@gmail.com>
2024-10-08 15:54:47 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if not s: 
+        if not s:  # Handle the empty string case explicitly
             return 0
-
+        
         roman_map = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000
@@ -16,11 +16,10 @@ class Solution(object):
         n = len(s)
         
         for i in range(n):
-
+            # If current numeral is smaller than the next, subtract its value
             if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
                 total -= roman_map[s[i]]
             else:
-
                 total += roman_map[s[i]]
         
         return total
