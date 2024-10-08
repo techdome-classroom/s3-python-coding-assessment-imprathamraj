63a7019ad3e3d585d4a3462d3e04be47d5275778
Pratham Raj <rjpratham04@gmail.com>
2024-10-08 15:49:16 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-
         roman_map = {
             'I': 1, 'V': 5, 'X': 10, 'L': 50,
             'C': 100, 'D': 500, 'M': 1000
@@ -13,18 +12,12 @@ class Solution(object):
         total = 0
         n = len(s)
         
-
         for i in range(n):
-
+            # If the current numeral is smaller than the next, subtract its value
             if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
                 total -= roman_map[s[i]]
             else:
-
+                # Otherwise, add its value
                 total += roman_map[s[i]]
         
         return total
-
-sol = Solution()
-print(sol.romanToInt("III"))   
-print(sol.romanToInt("LVIII"))  
-print(sol.romanToInt("MCMXCIV")) 
