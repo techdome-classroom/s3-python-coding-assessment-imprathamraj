90944a6f7ac3e37153092a09c9ed0e6783ae4ec0
Pratham Raj <rjpratham04@gmail.com>
2024-10-08 15:16:31 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        # Roman numeral mappings
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        # Iterate through the string
+        for i in range(n):
+            # If the current value is less than the next one, subtract it
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, add the value
+                total += roman_map[s[i]]
+        
+        return total
 
-
-
+# Example usage
+sol = Solution()
+print(sol.romanToInt("III"))      # Output: 3
+print(sol.romanToInt("LVIII"))    # Output: 58
+print(sol.romanToInt("MCMXCIV"))  # Output: 1994
