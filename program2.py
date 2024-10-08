41bc5d2662ba5df2df4f1cde888c6dc92f2637bd
Pratham Raj <rjpratham04@gmail.com>
2024-10-08 15:16:48 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -13,13 +13,13 @@ class Solution(object):
         total = 0
         n = len(s)
         
-        # Iterate through the string
+
         for i in range(n):
-            # If the current value is less than the next one, subtract it
+
             if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
                 total -= roman_map[s[i]]
             else:
-                # Otherwise, add the value
+
                 total += roman_map[s[i]]
         
         return total
